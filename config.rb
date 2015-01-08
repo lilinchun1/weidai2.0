05fc5bb5ee270cbd2f28f7470dee409ef853dc3e
llc <yxkj@163.com>
2015-01-08 17:27:54 +0800

    微代2.0 基本样式

---
+++ config.rb
@@ -0,0 +1,29 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "v20/css"
+sass_dir = "v20/sass"
+images_dir = "v20/img"
+javascripts_dir = "v20/js"
+font_dir = "v20/img/fonts"
+
+# enable sourcemaps
+enable_sourcemaps = true
+sass_options = {:sourcemap => true}
+
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+ relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
